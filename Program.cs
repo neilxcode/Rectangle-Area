aa9ec867464ca4af1d3e6a8868ac3b20d6c997a8
neilxcode <developros@icloud.com>
2017-05-10 22:37:29 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace rectangle
+{
+class CalculateArea
+    {
+        static int Area(int x, int y)
+        {
+            return x * y;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Width: ");
+            int intWidth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Length: ");
+            int intLength = Convert.ToInt32(Console.ReadLine());
+
+            int RectangleArea = Area(intWidth, intLength);
+
+            Console.WriteLine("The area of the rectangle is: {0}", RectangleArea);
+        }
+
+    }
+}
